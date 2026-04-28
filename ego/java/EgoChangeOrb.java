a4dc9ac20ce3548d28d90577431be4f0edc342a5
ydhappy <94909904+ydhappy@users.noreply.github.com>
2026-04-28 11:26:28 +0900

    Convert change orb item to ego orb compatibility wrapper

--- ego/java/EgoChangeOrb.java
+++ ego/java/EgoChangeOrb.java
@@ -1,31 +1,18 @@
 package lineage.world.object.item;
 
-import lineage.network.packet.ClientBasePacket;
-import lineage.world.object.Character;
-import lineage.world.controller.EgoChangeOrbController;
 import lineage.world.object.instance.ItemInstance;
-import lineage.world.object.instance.PcInstance;
 
 /**
- * 에고 변경구슬.
+ * 구버전 호환 wrapper.
  *
- * 사용 조건:
- * - 착용 중인 무기가 에고무기여야 한다.
- * - 사용 시 구슬 1개를 소모한다.
- * - 에고 능력과 대화 성향이 랜덤으로 재선택된다.
+ * 기존 에고 변경구슬 개념은 폐기한다.
+ * 실제 아이템 클래스는 EgoOrb를 사용한다.
  */
-public class EgoChangeOrb extends ItemInstance {
+public class EgoChangeOrb extends EgoOrb {
 
     static synchronized public ItemInstance clone(ItemInstance item) {
         if (item == null)
             item = new EgoChangeOrb();
         return item;
-    }
-
-    @Override
-    public void toClick(Character cha, ClientBasePacket cbp) {
-        if (!(cha instanceof PcInstance))
-            return;
-        EgoChangeOrbController.use((PcInstance) cha, this);
     }
 }
