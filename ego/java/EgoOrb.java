f9f650757cdd0b8e44fc15fdc1f133851a4bef08
ydhappy <94909904+ydhappy@users.noreply.github.com>
2026-04-28 11:51:54 +0900

    Route ego orb item through merged EgoCore

--- ego/java/EgoOrb.java
+++ ego/java/EgoOrb.java
@@ -1,21 +1,16 @@
 package lineage.world.object.item;
 
 import lineage.network.packet.ClientBasePacket;
+import lineage.world.controller.EgoCore;
 import lineage.world.object.Character;
-import lineage.world.controller.EgoOrbController;
 import lineage.world.object.instance.ItemInstance;
 import lineage.world.object.instance.PcInstance;
 
 /**
  * 에고 구슬.
  *
- * 사용 조건:
- * - 무기를 착용한 상태여야 한다.
- * - 착용 무기가 에고 대상 무기여야 한다.
- *
- * 동작:
- * - 에고가 없는 무기: 에고를 최초 생성한다.
- * - 이미 에고무기인 무기: 능력/대화/레벨을 변경하지 않고 현재 캐릭터를 주인으로 재인식한다.
+ * - 에고가 없는 착용 무기: 에고 최초 생성
+ * - 이미 에고무기인 착용 무기: 능력/대화/레벨 변경 없이 현재 캐릭터를 주인으로 재인식
  */
 public class EgoOrb extends ItemInstance {
 
@@ -29,6 +24,6 @@ public class EgoOrb extends ItemInstance {
     public void toClick(Character cha, ClientBasePacket cbp) {
         if (!(cha instanceof PcInstance))
             return;
-        EgoOrbController.use((PcInstance) cha, this);
+        EgoCore.useOrb((PcInstance) cha, this);
     }
 }
